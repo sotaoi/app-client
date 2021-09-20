83f0788814bc842cb5884ee020749024267a9161
qwertypnk <qwertypnk@protonmail.com>
2021-09-20 22:47:39 +0300

    chore: init

--- commands/user-commands.ts
+++
@@ -1,9 +0,0 @@
-import { ActionConclusion } from '@sotaoi/omni/transactions';
-import { Action } from '@sotaoi/client/action';
-import { store } from '@sotaoi/client/store';
-
-const removeUserCommand = async (uuid: string, role: null | string): Promise<ActionConclusion> => {
-  return Action.remove(store().getAccessToken(), role, 'user', uuid);
-};
-
-export { removeUserCommand };
